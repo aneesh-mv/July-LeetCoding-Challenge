8e64f4527adb2865fb999641fe5a808304129f1f
Aneesh MV <aneeshnairmv@gmail.com>
2020-07-19 23:44:10 +0530

    Add Binary

---
+++ Day19.cpp
@@ -0,0 +1,33 @@
+/*
+Given two binary strings, return their sum (also a binary string).
+
+The input strings are both non-empty and contains only characters 1 or 0.
+
+Example 1:
+
+Input: a = "11", b = "1"
+Output: "100"
+Example 2:
+
+Input: a = "1010", b = "1011"
+Output: "10101"
+*/
+class Solution
+{
+public:
+    string addBinary(string a, string b)
+    {
+        string s = "";
+        
+        int c = 0, i = a.size() - 1, j = b.size() - 1;
+        while(i >= 0 || j >= 0 || c == 1)
+        {
+            c += i >= 0 ? a[i --] - '0' : 0;
+            c += j >= 0 ? b[j --] - '0' : 0;
+            s += char(c % 2 + '0');
+            c /= 2;
+        }
+        reverse(s.begin(),s.end());
+        return s;
+    }
+};
